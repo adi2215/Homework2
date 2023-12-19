8b5961e052982b86ef8a044eaa2d2854e7b1ff6b
adi2215 <58622606+adi2215@users.noreply.github.com>
2023-12-19 10:05:30 +0600

    UpdateGame

---
+++ Assets/Scripts/CheckFlag.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckFlag : MonoBehaviour
+{
+    public GameObject image;
+    public GameObject player;
+    public int nextScene;
+    [SerializeField] private LevelLoadfer nextLevel;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            image.SetActive(true);
+            player.GetComponent<PlayerMovement>().enabled = false;
+            Invoke("NewScene", 1f);
+        }
+    }
+
+    private void NewScene()
+    {
+        nextLevel.LoadLevel(nextScene);
+    }
+}
